4dca905300ab4f58582498564c3a33db40845c32
Robert Bo Davis <bo@interrobang.consulting>
2024-03-11 10:35:13 -0400

    test: improve llm coverage

---
+++ core/src/llm/importClient.ts
@@ -0,0 +1,27 @@
+import type { Config, Provider } from '@/index.js';
+import { CodeLlmError } from '@/error/index.js';
+
+/**
+ * Import the provider module for a given provider
+ *
+ * @param provider - The provider to import
+ *
+ * @returns - The provider module
+ */
+export const importClient = async (config: Config, provider: Provider) => {
+  const providerConfigItem = config.providers[provider];
+
+  if (!providerConfigItem) {
+    return new CodeLlmError({
+      code: 'llm:invalidProvider',
+      meta: { provider },
+    });
+  }
+
+  const { config: providerConfig, module } = providerConfigItem;
+
+  return {
+    providerConfig,
+    providerModule: await import(module),
+  };
+};
